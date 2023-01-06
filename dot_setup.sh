6716e0fddbb8779af8cbb3fcbf3315804cb9aef6
nathan j <corigne@gmail.com>
2023-01-06 16:04:32 -0600

    forgot to newline output

--- dot_setup.sh
+++ dot_setup.sh
@@ -2,15 +2,15 @@
 
 #Creates the nvim config dir, installs vimplug, and installs plugins.
 cd $HOME
-printf 'Installing vimplug'
+printf "Installing vimplug...\n"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +qall
 
-printf "Adding nvim init.vim to .gitignore..."
+printf "Adding nvim init.vim to .gitignore...\n"
 cd $HOME/.config/nvim
 git update-index --assume-unchanged init.vim
 
-printf \n
-printf "neovim plugins installed... setup complete!"
+printf "\n"
+printf "neovim plugins installed... setup complete!\n"
