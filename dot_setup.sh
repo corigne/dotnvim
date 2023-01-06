3922fe8a247d66460e57798ff3583284d8eb5b57
nathan j <corigne@gmail.com>
2023-01-06 16:00:36 -0600

    switched from echo to printf for more consisten output across platforms

--- dot_setup.sh
+++ dot_setup.sh
@@ -2,15 +2,15 @@
 
 #Creates the nvim config dir, installs vimplug, and installs plugins.
 cd $HOME
-echo 'Installing vimplug'
+printf 'Installing vimplug'
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +qall
 
-echo "Adding nvim init.vim to .gitignore..."
+printf "Adding nvim init.vim to .gitignore..."
 cd $HOME/.config/nvim
 git update-index --assume-unchanged init.vim
 
-echo \n
-echo "neovim plugins installed... setup complete!"
+printf \n
+printf "neovim plugins installed... setup complete!"
